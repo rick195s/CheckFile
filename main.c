20d4555b2966ff51ba3a7c4261c85ea03c9e340b
rick195s <ricardosf195@gmail.com>
2021-10-02 21:11:51 +0100

    fileValidation v.0.2

--- main.c
+++ main.c
@@ -15,87 +15,128 @@
 #include "args.h"
 #include "debug.h"
 
+char *types[2] = {"text/plain", "text/plain"};
 //#include "memory.h"
 
+void getFileExtension(char* filePath){
+	printf("Extensao: %s\n", strrchr(filePath, '.')+1);
+
+}
+
 // Validate the file extension with the actual file type
-int mimeValidation(char *mimeType){
+int mimeValidation(char *mimeType)
+{
 
-
-		
-	printf("%s\n", mimeType);
-	return 0;
-}
-
-int mimeParsing(char *mime){
-	mimeValidation(mime);
+	printf("Mime: %s\n", mimeType);
+	mimeType = strrchr(mimeType, ':');
+	printf("O mime type: %s\n", mimeType + 2);
 
 	return 0;
 }
 
+int mimeParsing(FILE *fd, char *mime)
+{
+	// saves the file size in bytes
+	int fileSize = lseek(fileno(fd), 0L, SEEK_END);
 
-// Validate file permissions, existance, etc
-int fileValidation(char *filePath){
+	lseek(fileno(fd), 0L, SEEK_SET);
+
+	mime = realloc(mime,fileSize * sizeof(char));
+
+	// gets the fist line
+	fgets(mime, fileSize, fd);
+
+	return 0;
+}
+
+int fileProcessing(char *filePath)
+{
 
 	FILE *file = fopen("out.txt", "w+");
-	
-	if (file == NULL)
-		 ERROR(2,"Não foi possível criar o ficheiro \n\t'%s'", filePath);
+	char *mime = NULL;
 
-	// Redirecting the output to out.txt
-	dup2(fileno(file), STDOUT_FILENO);
-
+	mime = realloc(mime,0);
 
 	pid_t id = fork();
 
 	if (id == 0)
 	{
-		execlp("file","file","--mime-type",filePath,NULL);
-	}else
+		// Redirecting the output to out.txt
+		dup2(fileno(file), STDOUT_FILENO);
+		execlp("file", "file", "--mime-type", filePath, NULL);
+	}
+	else
 		wait(NULL);
 
-	dup2(STDOUT_FILENO, fileno(file));
+	mimeParsing(file, mime);
 
+	if(mime != NULL)
+		printf("Mime: %s\n", mime);
 
 	fclose(file);
-	return 0;	
+
+	getFileExtension(filePath);
+
+	free(mime);
+	
+	return 0;
+}
+
+// Validate file permissions, existance, etc
+int fileValidation(char *filePath)
+{
+	FILE *file = fopen(filePath, "r"); 
+
+	if (file == NULL)
+		ERROR(2, "Não foi possível abrir o ficheiro \n\t'%s'", filePath);
+
+	fclose(file);
+
+	fileProcessing(filePath);
+
+	return 0;
 }
 
 // Validate dir permissions, existance, etc
-int dirValidation(){
+int dirValidation()
+{
 
 	return 0;
 }
 
 // Validate if file have file paths in it
-int batchValidation(char *batchPath){
+int batchValidation(char *batchPath)
+{
 	fileValidation(batchPath);
 
 	return 0;
 }
 
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 
-    struct gengetopt_args_info args;
+	struct gengetopt_args_info args;
 
 	// gengetopt parser: deve ser a primeira linha de código no main
-	if(cmdline_parser(argc, argv, &args))
+	if (cmdline_parser(argc, argv, &args))
 		ERROR(1, "Erro: execução de cmdline_parser\n");
 
-	if(args.files_given > 0)
+	if (args.files_given > 0)
 		for (size_t i = 0; i < args.files_given; i++)
 		{
 			fileValidation(args.files_arg[i]);
 		}
-		
-	if(args.dir_given > 0)
+
+
+	if (args.dir_given > 0)
 		dirValidation(args.dir_arg);
-	
-	if(args.batch_given > 0)
+
+	if (args.batch_given > 0)
 		batchValidation(args.batch_arg);
-	
+
 	// gengetopt: libertar recurso (assim que possível)
 	cmdline_parser_free(&args);
 
-    return 0;
+	return 0;
 }

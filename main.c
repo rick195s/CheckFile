dc36eef9037ee5c37e36dbc2c52ea8b548693e6a
rick195s <ricardosf195@gmail.com>
2021-10-04 22:01:04 +0100

    fileProcessing v.0.3

--- main.c
+++ main.c
@@ -15,72 +15,7 @@
 #include "args.h"
 #include "debug.h"
 
-char *types[2] = {"text/plain", "text/plain"};
 //#include "memory.h"
-
-void getFileExtension(char* filePath){
-	printf("Extensao: %s\n", strrchr(filePath, '.')+1);
-
-}
-
-// Validate the file extension with the actual file type
-int mimeValidation(char *mimeType)
-{
-
-	printf("Mime: %s\n", mimeType);
-	mimeType = strrchr(mimeType, ':');
-	printf("O mime type: %s\n", mimeType + 2);
-
-	return 0;
-}
-
-int mimeParsing(FILE *fd, char *mime)
-{
-	// saves the file size in bytes
-	int fileSize = lseek(fileno(fd), 0L, SEEK_END);
-
-	lseek(fileno(fd), 0L, SEEK_SET);
-
-	mime = realloc(mime,fileSize * sizeof(char));
-
-	// gets the fist line
-	fgets(mime, fileSize, fd);
-
-	return 0;
-}
-
-int fileProcessing(char *filePath)
-{
-
-	FILE *file = fopen("out.txt", "w+");
-	char *mime = NULL;
-
-	mime = realloc(mime,0);
-
-	pid_t id = fork();
-
-	if (id == 0)
-	{
-		// Redirecting the output to out.txt
-		dup2(fileno(file), STDOUT_FILENO);
-		execlp("file", "file", "--mime-type", filePath, NULL);
-	}
-	else
-		wait(NULL);
-
-	mimeParsing(file, mime);
-
-	if(mime != NULL)
-		printf("Mime: %s\n", mime);
-
-	fclose(file);
-
-	getFileExtension(filePath);
-
-	free(mime);
-	
-	return 0;
-}
 
 // Validate file permissions, existance, etc
 int fileValidation(char *filePath)
@@ -88,11 +23,9 @@ int fileValidation(char *filePath)
 	FILE *file = fopen(filePath, "r"); 
 
 	if (file == NULL)
-		ERROR(2, "Não foi possível abrir o ficheiro \n\t'%s'", filePath);
+		ERROR(2, "Can't open file\n\t'%s'", filePath);
 
 	fclose(file);
-
-	fileProcessing(filePath);
 
 	return 0;
 }
@@ -113,6 +46,84 @@ int batchValidation(char *batchPath)
 }
 
 
+char *getFileExtension(char* filePath){
+
+	return strrchr(filePath, '.')+1;
+
+}
+
+// Validate the file extension with the actual file type
+int mimeValidation(char *mimeType, char *fileExtension)
+{
+	char *types[2] = {"application/pdf", "image/png"};
+	char *extensions[2] = {"pdf", "png"};
+
+	for (size_t i = 0; i < 2; i++)
+		if ( !strcmp(extensions[i], fileExtension) && !strcmp(types[i],mimeType) )
+			return 0;		
+	
+	return -1;
+}
+
+char *mimeParsing(FILE *fd, char *mimeType)
+{
+	// saves the file size in bytes
+	size_t fileSize = lseek(fileno(fd), 0, SEEK_END);
+
+	lseek(fileno(fd), 0, SEEK_SET);
+
+	// sizeof(char) = 1
+	mimeType = realloc(mimeType,fileSize);
+
+	// gets the fist line
+	fgets(mimeType, fileSize, fd);
+	
+	// finds the last occurence of ':' and adds 2
+	// so mimeType wont have ': ' 
+	mimeType = strrchr(mimeType, ':')+2;
+
+	return mimeType;
+}
+
+int fileProcessing(char *filePath)
+{
+	fileValidation(filePath);
+
+	FILE *outputFile = fopen("out.txt", "w+");
+	
+	char *mimeType = NULL;
+	char *fileExtension = NULL;
+
+	pid_t id = fork();
+
+	if (id == 0)
+	{
+		// Redirecting the output to out.txt
+		dup2(fileno(outputFile), STDOUT_FILENO);
+		execlp("file", "file", "--mime-type", filePath, NULL);
+	}
+	else
+		wait(NULL);
+
+	
+	mimeType = mimeParsing(outputFile, mimeType);
+	fclose(outputFile);
+
+	if(mimeType == NULL)
+		ERROR(2,"Not hable to request memory");
+
+	fileExtension = getFileExtension(filePath);
+	
+	if(!mimeValidation(mimeType, fileExtension)){
+		printf("[OK] '%s': extension '%s' matches file type '%s'\n", filePath, fileExtension,	mimeType);
+		return 0;
+	}
+	
+	printf("[MISMATCH] '%s': extension is '%s', file type is '%s'\n", filePath, fileExtension,	mimeType);
+
+	return -1;
+}
+
 int main(int argc, char *argv[])
 {
 
@@ -124,9 +135,8 @@ int main(int argc, char *argv[])
 
 	if (args.files_given > 0)
 		for (size_t i = 0; i < args.files_given; i++)
-		{
-			fileValidation(args.files_arg[i]);
-		}
+			fileProcessing(args.files_arg[i]);
+		
 
 
 	if (args.dir_given > 0)

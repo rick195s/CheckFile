b33e8f428e7bd4c2f4245159e421f86d48339d1e
rick195s <ricardosf195@gmail.com>
2021-10-14 20:43:55 +0100

    Validations v.0.1

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 /**
  * @file main.c
  * @brief Description
- * @date 2018-1-1
- * @author name of author
+ * @date 2021-10-5
+ * @author Ricardo dos Santos Franco 2202314
  */
 
 #include <stdio.h>
@@ -18,55 +18,54 @@
 #include "memory.h"
 
 #define MAX_EXT_SIZE 30
+#define MAX_FILENAME_SIZE 100
 
-// Validate file permissions, existance, etc
-FILE *openFile(char *filePath)
+int getFileExtension(char *fileExtension, const char *filePath);
+int extractFileType(FILE *outputFile, const char *filePath);
+int mimeValidation(const char *mimeType, const char *fileExtension, char *detectedExtension);
+char *mimeParsing(char *mimeType, const char *filePath);
+int fileProcessing(const char *filePath);
+int dirProcessing(char *dirPath);
+int batchProcessing(const char *batchPath);
+
+int getFileExtension(char *fileExtension, const char *filePath)
 {
-	FILE *file = fopen(filePath, "r");
+	char *ptr;
 
-	// Using exit will stop the program and not verify the other files.
-	// So I decided just to show the warning message and then return NULL or the file
+	// returns ptr if found '.' or NULL if not
+	if ((ptr = strrchr(filePath, '.')) == NULL)
+		return -1;
 
-	if (file == NULL)
-		fprintf(stderr, "ERROR: cannot open file '%s' -- %s\n", filePath, strerror(errno));
-
-	return file;
+	// I want was after the '.' character so +1
+	strcpy(fileExtension, ptr + 1);
+	return 0;
 }
 
-char *getFileExtension(char *fileExtension, char *filePath)
+int extractFileType(FILE *outputFile, const char *filePath)
 {
 
-	fileExtension = MALLOC(MAX_EXT_SIZE);
-
-	strcpy(fileExtension, strrchr(filePath, '.') + 1);
-
-	return fileExtension;
-}
-
-void extractFileType(FILE *outputFile, char *filePath)
-{
 	// Creates child process
 	pid_t id = fork();
 
 	if (id == 0)
 	{
-		// Redirecting the output to out.txt
+		// Redirecting the child output to out.txt
 		dup2(fileno(outputFile), STDOUT_FILENO);
 		execlp("file", "file", "--mime-type", filePath, NULL);
 	}
 	else
 		// Parent waits for Child
 		wait(NULL);
+
+	return 0;
 }
 
 // Validate the file extension with the actual file type
-int mimeValidation(char *mimeType, char *fileExtension, char *detectedExtension)
+int mimeValidation(const char *mimeType, const char *fileExtension, char *detectedExtension)
 {
 	// define the supported mime types and extensions
 	char *types[3] = {"application/pdf", "image/png", "image/jpeg"};
 	char *extensions[3] = {"pdf", "png", "jpeg/jpg/jpe/jfif"};
-
-	detectedExtension = MALLOC(MAX_EXT_SIZE);
 
 	strcpy(detectedExtension, "");
 
@@ -86,11 +85,15 @@ int mimeValidation(char *mimeType, char *fileExtension, char *detectedExtension)
 	return -1;
 }
 
-char *mimeParsing(char *mimeType, char *filePath)
+char *mimeParsing(char *mimeType, const char *filePath)
 {
 	FILE *outputFile = fopen("out.txt", "w+");
 
-	extractFileType(outputFile, filePath);
+	if (outputFile == NULL)
+		return NULL;
+
+	if (extractFileType(outputFile, filePath) < 0)
+		return NULL;
 
 	// saves the file size in bytes
 	size_t fileSize = lseek(fileno(outputFile), 0, SEEK_END);
@@ -98,39 +101,47 @@ char *mimeParsing(char *mimeType, char *filePath)
 	lseek(fileno(outputFile), 0, SEEK_SET);
 
 	// sizeof(char) = 1
-	//mimeType = realloc(mimeType, fileSize);
-	mimeType = MALLOC(fileSize);
+	mimeType = MALLOC(fileSize * sizeof(char));
 
 	if (mimeType == NULL)
-		ERROR(2, "Not hable to request memory");
+	{
+		fclose(outputFile);
+		fprintf(stderr, "[ERROR]: not hable to allocate memory\n");
+		exit(2);
+	}
 
 	// gets the fist line
-	fgets(mimeType, fileSize, outputFile);
+	fgets(mimeType, (int)fileSize, outputFile);
 
 	// finds the last occurence of ':' and adds 2
 	// so mimeType wont have ': '
-
 	strcpy(mimeType, strrchr(mimeType, ':') + 2);
-
 	fclose(outputFile);
 
 	return mimeType;
 }
 
-int fileProcessing(char *filePath)
+int fileProcessing(const char *filePath)
 {
-	FILE *file = openFile(filePath);
-
-	if (file == NULL)
+	// Checking file permissions
+	int asPerm = access(filePath, R_OK);
+	if (asPerm != 0)
+	{
+		fprintf(stderr, "[ERROR]: cannot open file '%s' -- %s\n", filePath, strerror(errno));
 		return -1;
+	}
 
 	char *mimeType = NULL;
-	char *fileExtension = NULL;
-	char *detectedExtension = NULL;
+	char *fileExtension = MALLOC(MAX_EXT_SIZE);
+	char *detectedExtension = MALLOC(MAX_EXT_SIZE);
 
 	mimeType = mimeParsing(mimeType, filePath);
 
-	fileExtension = getFileExtension(fileExtension, filePath);
+	if (mimeType == NULL)
+		return -1;
+
+	if (getFileExtension(fileExtension, filePath))
+		return -1;
 
 	// Show output information
 	switch (mimeValidation(mimeType, fileExtension, detectedExtension))
@@ -151,10 +162,9 @@ int fileProcessing(char *filePath)
 		break;
 	}
 
-	fclose(file);
 	FREE(detectedExtension);
-	FREE(mimeType);
 	FREE(fileExtension);
+	FREE(mimeType);
 
 	return 0;
 }
@@ -162,23 +172,32 @@ int fileProcessing(char *filePath)
 // Validate dir permissions, existance, etc
 int dirProcessing(char *dirPath)
 {
+
 	DIR *dir = opendir(dirPath);
 	struct dirent *dir_entry;
 	int finito = 0;
 	char *fullPath = NULL;
 
-	// Check last char of dirPath
+	if (dir == NULL)
+	{
+		fprintf(stderr, "[ERROR]: cannot open dir '%s' -- %s\n", dirPath, strerror(errno));
+		return -1;
+	}
+
+	// Checking last char of dirPath
 	// Using values of ASCII table to avoid calling strcomp because
 	// I'm just comparing char
 	if (dirPath[strlen(dirPath) - 1] != (int)'/')
 	{
-		strcat(dirPath, "/");
-	}
+		// Adding space for '/' to the string
+		dirPath = realloc(dirPath, 1);
+		if (dirPath == NULL)
+		{
+			closedir(dir);
+			fprintf(stderr, "[ERROR]: ");
+		}
 
-	if (dir == NULL)
-	{
-		fprintf(stderr, "ERROR: cannot open dir '%s' -- %s\n", dirPath, strerror(errno));
-		return -1;
+		strcat(dirPath, "/");
 	}
 
 	while (finito == 0)
@@ -196,47 +215,67 @@ int dirProcessing(char *dirPath)
 		}
 		else
 		{
-			fullPath = MALLOC(strlen(dirPath) + strlen(dir_entry->d_name));
+			// +1 for the terminator '\0'
+			fullPath = MALLOC(strlen(dirPath) + strlen(dir_entry->d_name) + 1);
 			if (fullPath == NULL)
 			{
-				fprintf(stderr, "ERROR: cannot allocate memory %s\n", strerror(errno));
+				fprintf(stderr, "[ERROR]: cannot allocate memory\n");
 				return -1;
 			}
 
+			// Initializing fullPath
+			strcpy(fullPath, "");
 			strcat(fullPath, dirPath);
-			strcat(fullPath, dir_entry->d_name);
+			strcat(fullPath, (dir_entry->d_name));
 			fileProcessing(fullPath);
+			FREE(fullPath);
 		}
 	}
 
 	if (closedir(dir) == -1)
 	{
-		fprintf(stderr, "ERROR: cannot close dir '%s' -- %s\n",
+		fprintf(stderr, "[ERROR]: cannot close dir '%s' -- %s\n",
 				dirPath, strerror(errno));
+
 		return -1;
 	}
 
-	FREE(fullPath);
 	return 0;
 }
 
 // Validate if file have file paths in it
-int batchProcessing(char *batchPath)
+int batchProcessing(const char *batchPath)
 {
-	FILE *file = openFile(batchPath);
+
+	FILE *file = fopen(batchPath, "r");
 
 	if (file == NULL)
+	{
+		fprintf(stderr, "[ERROR]: cannot open file '%s' -- %s\n", batchPath, strerror(errno));
 		return -1;
+	}
 
-	char *fileToVal = MALLOC(100);
+	char *fileToVal = MALLOC(MAX_FILENAME_SIZE);
 
-	// "ler" até ao fim do ficheiro
+	// Read from file until EOF
 	while (!feof(file))
 	{
-		if (fgets(fileToVal, 100, file) != NULL)
+		if (ferror(file))
 		{
-			// remove \n char from string
-			fileToVal = strtok(fileToVal, "\n");
+			fprintf(stderr, "[ERROR]: not hable to read from file\n");
+			return -1;
+		}
+
+		if (fgets(fileToVal, MAX_FILENAME_SIZE, file) != NULL)
+		{
+			// if file has length of 1 it means that the only char is '\n'
+			if (strlen(fileToVal) <= 1)
+				continue;
+
+			// Removes \n char from string
+			if (strrchr(fileToVal, '\n') != NULL)
+				strtok(fileToVal, "\n");
+
 			fileProcessing(fileToVal);
 		}
 	}

2197730db6a208e807dfb6187512a18a69e94298
rick195s <ricardosf195@gmail.com>
2021-10-18 17:01:33 +0100

    summary and errors

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # CheckFile
 
 A C program that will check if a file really matches his own extension
+
+Preferi passar o summary por parametro para registar os status dos ficheiros em vez de alterar os valores de summary com o valor de retorno de fileProcessing nas funcoes de dirProcessing e batchProcessing pois se o fizesse iria estar a adicionar mais comparações desnecessárias ao programa para verificar se o retorno da funcao era porque o ficheiro era mismatch, ok ou erro.
+
+Utilizei a alocacao de memoria dinamica pois queria tornar o programa um pouco mais eficiente.
+
+In fileProcessing im using "access()" function because I dont want to open the file, I just want to verify if the bash program "file" will have access to read from it 
+
--- main.c
+++ main.c
@@ -19,15 +19,41 @@
 
 #define MAX_EXT_SIZE 30
 #define MAX_FILENAME_SIZE 100
+#define EXT_NUMBER 3
 
 int getFileExtension(char *fileExtension, const char *filePath);
-int extractFileType(FILE *outputFile, const char *filePath);
+void extractFileType(FILE *outputFile, const char *filePath);
 int mimeValidation(const char *mimeType, const char *fileExtension, char *detectedExtension);
 char *mimeParsing(char *mimeType, const char *filePath);
-int fileProcessing(const char *filePath);
-int dirProcessing(char *dirPath);
-int batchProcessing(const char *batchPath);
+int fileProcessing(const char *filePath, int *summary);
+int dirProcessing(char *dirPath, int *summary);
+int batchProcessing(const char *batchPath, int *summary);
+void showSummary(const int *summary);
 
+/**
+ * Show summary of processed files
+ * @param summary array with 3 positions (OK, MISMATCH, ERROR)
+ * @return Nothing returned
+ */
+void showSummary(const int *summary)
+{
+	int total = 0;
+
+	for (size_t i = 0; i < 3; i++)
+	{
+		total += *(summary + i);
+	}
+
+	printf("[SUMMARY] files analyzed: %d; files OK: %d; Mismatch: %d; Errors: %d\n", total, *summary, *(summary + 1), *(summary + 2));
+}
+
+/**
+ * Gets the file extension
+ * @param fileExtension string where the extension will be copied
+ * @param filePath path to the file 
+ * @return	0 -> extension was detected;
+ * 			-1 -> extension not detected
+ */
 int getFileExtension(char *fileExtension, const char *filePath)
 {
 	char *ptr;
@@ -36,12 +62,18 @@ int getFileExtension(char *fileExtension, const char *filePath)
 	if ((ptr = strrchr(filePath, '.')) == NULL)
 		return -1;
 
-	// I want was after the '.' character so +1
+	// I want what's after the '.' character so +1
 	strcpy(fileExtension, ptr + 1);
 	return 0;
 }
 
-int extractFileType(FILE *outputFile, const char *filePath)
+/**
+ * Extracts the original file type and stores the output inside outputFile
+ * @param outputFile pointer to FILE struct where file type of filePath will be stored
+ * @param filePath path to the file 
+ * @return Nothing returned
+ */
+void extractFileType(FILE *outputFile, const char *filePath)
 {
 
 	// Creates child process
@@ -56,20 +88,26 @@ int extractFileType(FILE *outputFile, const char *filePath)
 	else
 		// Parent waits for Child
 		wait(NULL);
-
-	return 0;
 }
 
-// Validate the file extension with the actual file type
+/**
+ * Validates the file extension with the actual file type
+ * @param mimeType string where the mime type detected by the bash program "file" is stored
+ * @param fileExtension string where the extracted extension from the file path string is stored
+ * @param detectedExtension string where actual extension, if detected, will be stored
+ * @return 	0 -> extensions are compatible;
+ * 			-1 -> mime supported but is a mismatch;
+ * 			-2 -> mime not supported
+ */
 int mimeValidation(const char *mimeType, const char *fileExtension, char *detectedExtension)
 {
 	// define the supported mime types and extensions
-	char *types[3] = {"application/pdf", "image/png", "image/jpeg"};
-	char *extensions[3] = {"pdf", "png", "jpeg/jpg/jpe/jfif"};
+	char *types[EXT_NUMBER] = {"application/pdf", "image/png", "image/jpeg"};
+	char *extensions[EXT_NUMBER] = {"pdf", "png", "jpeg/jpg/jpe/jfif"};
 
 	strcpy(detectedExtension, "");
 
-	for (size_t i = 0; i < 3; i++)
+	for (size_t i = 0; i < EXT_NUMBER; i++)
 		// Check if mimeType is supported by checkFile
 		if (!strcmp(types[i], mimeType))
 		{
@@ -78,13 +116,19 @@ int mimeValidation(const char *mimeType, const char *fileExtension, char *detect
 				return 0;
 		}
 
-	// if detectedExtension is empty then the mime type isn't supported
+	// if detectedExtension is empty string then the mime type isn't supported
 	if (!strcmp(detectedExtension, ""))
 		return -2;
 
 	return -1;
 }
 
+/**
+ * Analyzes the mime of filePath
+ * @param mimeType string where the mime type detected by the bash program "file" will be stored
+ * @param filePath path to the file 
+ * @return 	pointer to memory for string with the mime type or NULL
+ */
 char *mimeParsing(char *mimeType, const char *filePath)
 {
 	FILE *outputFile = fopen("out.txt", "w+");
@@ -106,7 +150,7 @@ char *mimeParsing(char *mimeType, const char *filePath)
 	if (mimeType == NULL)
 	{
 		fclose(outputFile);
-		fprintf(stderr, "[ERROR]: not hable to allocate memory\n");
+		fprintf(stderr, "[ERROR] not hable to allocate memory\n");
 		exit(2);
 	}
 
@@ -121,13 +165,22 @@ char *mimeParsing(char *mimeType, const char *filePath)
 	return mimeType;
 }
 
-int fileProcessing(const char *filePath)
+/**
+ * Start of processing the file
+ * @param filePath path to the file 
+ * @param summary array with 3 positions (OK, MISMATCH, ERROR)
+ * @return 	0 -> all ok;
+ * 			-1 -> error detected
+ */
+int fileProcessing(const char *filePath, int *summary)
 {
 	// Checking file permissions
 	int asPerm = access(filePath, R_OK);
 	if (asPerm != 0)
 	{
-		fprintf(stderr, "[ERROR]: cannot open file '%s' -- %s\n", filePath, strerror(errno));
+		fprintf(stderr, "[ERROR] cannot open file '%s' -- %s\n", filePath, strerror(errno));
+		(*(summary + 2))++;
+
 		return -1;
 	}
 
@@ -148,10 +201,12 @@ int fileProcessing(const char *filePath)
 	{
 	case 0:
 		printf("[OK] '%s': extension '%s' matches file type '%s'\n", filePath, fileExtension, detectedExtension);
+		(*summary)++;
 		break;
 
 	case -1:
 		printf("[MISMATCH] '%s': extension is '%s', file type is '%s'\n", filePath, fileExtension, detectedExtension);
+		(*(summary + 1))++;
 		break;
 
 	case -2:
@@ -162,6 +217,7 @@ int fileProcessing(const char *filePath)
 		break;
 	}
 
+	// Freeing memory
 	FREE(detectedExtension);
 	FREE(fileExtension);
 	FREE(mimeType);
@@ -169,38 +225,47 @@ int fileProcessing(const char *filePath)
 	return 0;
 }
 
-// Validate dir permissions, existance, etc
-int dirProcessing(char *dirPath)
+/**
+ * Analysing the directory files 
+ * @param dirPath string to the directory
+ * @param summary array with 3 positions (OK, MISMATCH, ERROR)
+ * @return 	0 -> all ok;
+ * 			-1 -> error detected
+ */
+int dirProcessing(char *dirPath, int *summary)
 {
-
 	DIR *dir = opendir(dirPath);
 	struct dirent *dir_entry;
-	int finito = 0;
+	int fim = 0;
 	char *fullPath = NULL;
 
 	if (dir == NULL)
 	{
-		fprintf(stderr, "[ERROR]: cannot open dir '%s' -- %s\n", dirPath, strerror(errno));
+		fprintf(stderr, "[ERROR] cannot open dir '%s' -- %s\n", dirPath, strerror(errno));
 		return -1;
 	}
 
 	// Checking last char of dirPath
-	// Using values of ASCII table to avoid calling strcomp because
-	// I'm just comparing char
-	if (dirPath[strlen(dirPath) - 1] != (int)'/')
+	// Using values of ASCII table to avoid calling strcomp
+	if ((int)dirPath[strlen(dirPath) - 1] != (int)'/')
 	{
 		// Adding space for '/' to the string
 		dirPath = realloc(dirPath, 1);
+
 		if (dirPath == NULL)
 		{
 			closedir(dir);
-			fprintf(stderr, "[ERROR]: ");
+			fprintf(stderr, "[ERROR] not hable to allocate memory\n");
+			return -1;
 		}
 
 		strcat(dirPath, "/");
 	}
 
-	while (finito == 0)
+	printf("[INFO] analyzing files of directory '%s'\n", dirPath);
+
+	// Read from dir
+	while (fim == 0)
 	{
 		dir_entry = readdir(dir);
 		if (dir_entry == NULL)
@@ -211,7 +276,7 @@ int dirProcessing(char *dirPath)
 				return -1;
 			}
 			else
-				finito = 1;
+				fim = 1;
 		}
 		else
 		{
@@ -219,22 +284,23 @@ int dirProcessing(char *dirPath)
 			fullPath = MALLOC(strlen(dirPath) + strlen(dir_entry->d_name) + 1);
 			if (fullPath == NULL)
 			{
-				fprintf(stderr, "[ERROR]: cannot allocate memory\n");
+				fprintf(stderr, "[ERROR] cannot allocate memory\n");
 				return -1;
 			}
 
-			// Initializing fullPath
-			strcpy(fullPath, "");
+			// Initializing 'fullPath'
+			memset(fullPath, '\0', 1);
+			//strcpy(fullPath, "");
 			strcat(fullPath, dirPath);
 			strcat(fullPath, (dir_entry->d_name));
-			fileProcessing(fullPath);
+			fileProcessing(fullPath, summary);
 			FREE(fullPath);
 		}
 	}
 
 	if (closedir(dir) == -1)
 	{
-		fprintf(stderr, "[ERROR]: cannot close dir '%s' -- %s\n",
+		fprintf(stderr, "[ERROR] cannot close dir '%s' -- %s\n",
 				dirPath, strerror(errno));
 
 		return -1;
@@ -243,26 +309,35 @@ int dirProcessing(char *dirPath)
 	return 0;
 }
 
-// Validate if file have file paths in it
-int batchProcessing(const char *batchPath)
+/**
+ * Analysing the files listed inside btachPath 
+ * @param btachPath string to the file
+ * @param summary array with 3 positions (OK, MISMATCH, ERROR)
+ * @return 	0 -> all ok;
+ * 			-1 -> error detected
+ */
+int batchProcessing(const char *batchPath, int *summary)
 {
-
 	FILE *file = fopen(batchPath, "r");
 
 	if (file == NULL)
 	{
-		fprintf(stderr, "[ERROR]: cannot open file '%s' -- %s\n", batchPath, strerror(errno));
+		fprintf(stderr, "[ERROR] cannot open file '%s' -- %s\n", batchPath, strerror(errno));
 		return -1;
 	}
 
+	printf("[INFO] analyzing files listed in '%s'\n", batchPath);
+
 	char *fileToVal = MALLOC(MAX_FILENAME_SIZE);
+	int fileError = 0;
 
 	// Read from file until EOF
 	while (!feof(file))
 	{
-		if (ferror(file))
+		fileError = ferror(file);
+		if (fileError)
 		{
-			fprintf(stderr, "[ERROR]: not hable to read from file\n");
+			fprintf(stderr, "[ERROR] cannot read from file or dir '%s' -- %s\n", batchPath, strerror(fileError));
 			return -1;
 		}
 
@@ -276,7 +351,7 @@ int batchProcessing(const char *batchPath)
 			if (strrchr(fileToVal, '\n') != NULL)
 				strtok(fileToVal, "\n");
 
-			fileProcessing(fileToVal);
+			fileProcessing(fileToVal, summary);
 		}
 	}
 
@@ -290,6 +365,7 @@ int main(int argc, char *argv[])
 {
 
 	struct gengetopt_args_info args;
+	int summary[3] = {0};
 
 	// gengetopt parser: deve ser a primeira linha de código no main
 	if (cmdline_parser(argc, argv, &args))
@@ -297,13 +373,19 @@ int main(int argc, char *argv[])
 
 	if (args.files_given > 0)
 		for (size_t i = 0; i < args.files_given; i++)
-			fileProcessing(args.files_arg[i]);
+			fileProcessing(args.files_arg[i], summary);
 
 	if (args.dir_given > 0)
-		dirProcessing(args.dir_arg);
+	{
+		dirProcessing(args.dir_arg, summary);
+		showSummary(summary);
+	}
 
 	if (args.batch_given > 0)
-		batchProcessing(args.batch_arg);
+	{
+		batchProcessing(args.batch_arg, summary);
+		showSummary(summary);
+	}
 
 	// gengetopt: libertar recurso (assim que possível)
 	cmdline_parser_free(&args);
